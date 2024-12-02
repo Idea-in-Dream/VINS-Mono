b80d0e4007b9be2268c55756bee5062580ea0858
cuimingdi <1359136134@qq.com>
2024-12-02 21:00:06 +0800

    适配opencv4

--- pose_graph/src/ThirdParty/DVision/BRIEF.h
+++ pose_graph/src/ThirdParty/DVision/BRIEF.h
@@ -31,6 +31,9 @@
 
 #include <opencv2/opencv.hpp>
 #include <vector>
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/imgproc/imgproc_c.h>
+
 #include <boost/dynamic_bitset.hpp>
 
 namespace DVision {
